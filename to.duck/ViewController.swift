a5a0c0468b023e9b4ba706e2461f8a1f95d86327
Kyxxn <gywns626@naver.com>
2024-03-01 18:50:48 +0900

    [ADD] No StoryBoard First init

---
+++ to.duck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  to.duck
+//
+//  Created by 박효준 on 3/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+    }
+
+
+}
+
